4d6564575ba55fdb54af358c902e7d0786dccbc4
alireza1998 <alireza.eidani1000@gmail.com>
2016-01-17 22:47:06 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -221,29 +221,17 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {122835592,159887854,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
 An advance Administration bot based on yagop/telegram-bot 
 
-https://github.com/SEEDTEAM/TeleSeed
+
 
 Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@Rondoozle [Developer]
-@seyedan25 [Manager]
-
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
-
-Our channels
-@teleseedch [English]
-@iranseed [persian]
+@alireza_PT [Developer]
+@alireza_PT [Manager]
 ]],
     help_text_realm = [[
 Realm Commands:
@@ -320,110 +308,122 @@ This command will send text to [group_id]
 *Only admins and sudo can use res, setowner, commands
 ]],
     help_text = [[
-Commands list :
+لیست دستورات :
 
-!kick [username|id]
-You can also do it by reply
+[/!]kick [username|id]
+({اخراج کردن افراد از گروه{قابل انجام با ریپلی)
 
-!ban [ username|id]
-You can also do it by reply
+[/!]ban [ username|id]
+({بن کردن افراد از گروه{قابل انجام با ریپلی)
 
-!unban [id]
-You can also do it by reply
+[/!]unban [id]
+({خارج کردن افراد از حالت بن در گروه{قابل انجام با ریپلی)
 
-!who
-Members list
+[/!]banall [id]
+(بن کردن فرد مورد نظر از تمامی گروه های ربات)
 
-!modlist
-Moderators list
+[/!]unbanall [id]
+(خارج کردن فرد مورد نظر از محرومیت ورود به گروه های کل ربات)
 
-!promote [username]
-Promote someone
+[/!]who
+(لیست افراد موجود در گروه)
 
-!demote [username]
-Demote someone
+[/!]modlist
+(لیست مدیران موجود در گروه)
 
-!kickme
-Will kick user
+[/!]promote [username]
+(ارتقاء مقام به عنوان مدیریت )
 
-!about
-Group description
+[/!]demote [username]
+(سلب مقام مدیریت)
 
-!setphoto
-Set and locks group photo
+[/!]kickme
+(اخراج کردن من از گروه)
 
-!setname [name]
-Set group name
+[/!]about
+(اطلاعات درباره گروه)
 
-!rules
-Group rules
+[/!]setphoto
+(تنظیم و قفل کردن عکس گروه)
 
-!id
-return group id or user id
+[/!]setname [name]
+(تنظیم نام برای گروه)
 
-!help
+[/!]rules
+(قوانین گروه)
 
-!lock [member|name|bots|leave]	
-Locks [member|name|bots|leaveing] 
+[/!]id
+({آیدی گروه و افراد{قابل انجام با ریپلی)
 
-!unlock [member|name|bots|leave]
-Unlocks [member|name|bots|leaving]
-
-!set rules <text>
-Set <text> as rules
-
-!set about <text>
-Set <text> as about
-
-!settings
-Returns group settings
-
-!newlink
-create/revoke your group link
-
-!link
-returns group link
-
-!owner
-returns group owner id
-
-!setowner [id]
-Will set id as owner
-
-!setflood [value]
-Set [value] as flood sensitivity
-
-!stats
-Simple message statistics
-
-!save [value] <text>
-Save <text> as [value]
-
-!get [value]
-Returns text of [value]
-
-!clean [modlist|rules|about]
-Will clear [modlist|rules|about] and set it to nil
-
-!res [username]
-returns user id
-"!res @username"
-
-!log
-will return group logs
-
-!banlist
-will return group ban list
-
-**U can use both "/" and "!" 
+[/!]lock [member|name|bots|arabic|leave]	
+(قفل کردن [ورود به گروه|نام گروه|ورود ربات ها|عربی|خروج از گروه])
 
 
-*Only owner and mods can add bots in group
+[/!]unlock [member|name|bots|leave]
+(باز کردن [ورود به گروه|نام گروه|ورود ربات ها|عربی|خروج از گروه])
 
+[/!]set rules <text>
+(تنظیم کردن <متن> برای قوانین گروه)
 
-*Only moderators and owner can use kick,ban,unban,newlink,link,setphoto,setname,lock,unlock,set rules,set about and settings commands
+[/!]set about <text>
+(تنظیم کردن <متن> برای درباره گروه)
 
-*Only owner can use res,setowner,promote,demote and log commands
+[/!]settings
+(تنظیمات گروه)
+
+[/!]newlink
+(ساختن/غیر فعال سازی لینک برای گروه)
+
+[/!]link
+(نمایش دادن لینک گروه)
+
+[/!]linkpv
+(PVارسال لینک گروه به)
+
+[/!]echo <text>
+(قابلیت تکرار کردن متن مورد نظر توسط ربات)
+
+[/!]owner
+(نمایش دادن اونر گروه)
+
+[/!]setowner [id]
+(تنظیم اونر برای گروه)
+
+[/!]setflood [value]
+(تنظیم حساسیت نسبت به اسپم)
+
+[/!]stats
+(اطلاعات چت های گروه)
+
+[/!]save [value] <text>
+(ذخیره <متن> و [عدد])
+
+[/!]get [value]
+(رجوع به [عدد])
+
+[/!]clean [modlist|rules|about]
+(پاک کردن[مدیران|قوانین|درباره گروه|افراد] گروه)
+
+[/!]res [username]
+([درباره [آیدی فرد مورد نظر)
+
+[/!]time [local]
+(تاریخ و ساعت لحظه ای وقعیت مورد نظر شما)
+
+[/!]calc {÷,×,-,+}
+(ماشین حساب پیشرفته)
+
+[/!]conv <text>
+(تبدیل متن به عکس)
+
+[/!]log
+(آمار ورود و خروج)
+
+[/!]tagall <text>
+(ارسال متن مورد نظر به تمام آیدی های موجود در گروه)
+
+[/!]banlist
+(لیست افراد بن شده)
 
 ]]
   }
